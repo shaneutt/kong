3091c4f3c150391d85359a27f21eeef9fbc011a5
mikefero <michael.fero@konghq.com>
2021-12-17 12:13:01 -0500

    chore(test) ensure ACL tests are skipped for unsupported Redis versions
    Redis v5.x has reached EOL; however security patches are still being
    made to this release line. A deprecation warning should be added when
    Redis < v6.x is detected.

--- spec/03-plugins/23-rate-limiting/05-integration_spec.lua
+++ spec/03-plugins/23-rate-limiting/05-integration_spec.lua
@@ -1,5 +1,7 @@
 local helpers = require "spec.helpers"
 local redis = require "resty.redis"
+local version = require "version"
+local tostring = tostring
 
 
 local REDIS_HOST = helpers.redis_host
@@ -15,37 +17,34 @@ local REDIS_PASSWORD = "secret"
 
 local SLEEP_TIME = 1
 
-
-local function flush_redis(db)
+local function redis_connect()
   local red = redis:new()
   red:set_timeout(2000)
   assert(red:connect(REDIS_HOST, REDIS_PORT))
+  local red_version = string.match(red:info(), 'redis_version:([%d.]+)\r\n')
+  return red, assert(version(red_version))
+end
+
+local function flush_redis(red, db)
   assert(red:select(db))
   red:flushall()
-  red:close()
 end
 
-local function add_redis_user()
-  local red = redis:new()
-  red:set_timeout(2000)
-  assert(red:connect(REDIS_HOST, REDIS_PORT))
+local function add_redis_user(red)
   assert(red:acl("setuser", REDIS_USER_VALID, "on", "allkeys", "+incrby", "+select", "+info", "+expire", "+get", ">" .. REDIS_PASSWORD))
   assert(red:acl("setuser", REDIS_USER_INVALID, "on", "allkeys", "+get", ">" .. REDIS_PASSWORD))
-  red:close()
 end
 
-local function remove_redis_user()
-  local red = redis:new()
-  red:set_timeout(2000)
-  assert(red:connect(REDIS_HOST, REDIS_PORT))
+local function remove_redis_user(red)
   assert(red:acl("deluser", REDIS_USER_VALID))
   assert(red:acl("deluser", REDIS_USER_INVALID))
-  red:close()
 end
 
 describe("Plugin: rate-limiting (integration)", function()
   local client
   local bp
+  local red
+  local red_version
 
   lazy_setup(function()
     bp = helpers.get_db_utils(nil, {
@@ -55,11 +54,15 @@ describe("Plugin: rate-limiting (integration)", function()
     }, {
       "rate-limiting"
     })
+    red, red_version = redis_connect()
   end)
 
   lazy_teardown(function()
     if client then
       client:close()
+    end
+    if red then
+      red:close()
     end
 
     helpers.stop_kong()
@@ -70,9 +73,11 @@ describe("Plugin: rate-limiting (integration)", function()
     -- https://github.com/Kong/kong/issues/3292
 
     lazy_setup(function()
-      flush_redis(REDIS_DB_1)
-      flush_redis(REDIS_DB_2)
-      add_redis_user()
+      flush_redis(red, REDIS_DB_1)
+      flush_redis(red, REDIS_DB_2)
+      if red_version > version("6.0.0") then
+        add_redis_user(red)
+      end
 
       local route1 = assert(bp.routes:insert {
         hosts        = { "redistest1.com" },
@@ -106,41 +111,43 @@ describe("Plugin: rate-limiting (integration)", function()
         }
       })
 
-      local route3 = assert(bp.routes:insert {
-        hosts        = { "redistest3.com" },
-      })
-      assert(bp.plugins:insert {
-        name = "rate-limiting",
-        route = { id = route3.id },
-        config = {
-          minute         = 1,
-          policy         = "redis",
-          redis_host     = REDIS_HOST,
-          redis_port     = REDIS_PORT,
-          redis_username = REDIS_USER_VALID,
-          redis_password = REDIS_PASSWORD,
-          redis_database = 3, -- ensure to not get a pooled authenticated connection by using a different db
-          fault_tolerant = false,
-        }
-      })
+      if red_version >= version("6.0.0") then
+        local route3 = assert(bp.routes:insert {
+          hosts        = { "redistest3.com" },
+        })
+        assert(bp.plugins:insert {
+          name = "rate-limiting",
+          route = { id = route3.id },
+          config = {
+            minute         = 1,
+            policy         = "redis",
+            redis_host     = REDIS_HOST,
+            redis_port     = REDIS_PORT,
+            redis_username = REDIS_USER_VALID,
+            redis_password = REDIS_PASSWORD,
+            redis_database = 3, -- ensure to not get a pooled authenticated connection by using a different db
+            fault_tolerant = false,
+          }
+        })
 
-      local route4 = assert(bp.routes:insert {
-        hosts        = { "redistest4.com" },
-      })
-      assert(bp.plugins:insert {
-        name = "rate-limiting",
-        route = { id = route4.id },
-        config = {
-          minute         = 1,
-          policy         = "redis",
-          redis_host     = REDIS_HOST,
-          redis_port     = REDIS_PORT,
-          redis_username = REDIS_USER_INVALID,
-          redis_password = REDIS_PASSWORD,
-          redis_database = 4, -- ensure to not get a pooled authenticated connection by using a different db                  
-          fault_tolerant = false,
-        }
-      })
+        local route4 = assert(bp.routes:insert {
+          hosts        = { "redistest4.com" },
+        })
+        assert(bp.plugins:insert {
+          name = "rate-limiting",
+          route = { id = route4.id },
+          config = {
+            minute         = 1,
+            policy         = "redis",
+            redis_host     = REDIS_HOST,
+            redis_port     = REDIS_PORT,
+            redis_username = REDIS_USER_INVALID,
+            redis_password = REDIS_PASSWORD,
+            redis_database = 4, -- ensure to not get a pooled authenticated connection by using a different db
+            fault_tolerant = false,
+          }
+        })
+      end
 
 
       assert(helpers.start_kong({
@@ -150,19 +157,12 @@ describe("Plugin: rate-limiting (integration)", function()
     end)
 
     lazy_teardown(function()
-      remove_redis_user()
+      if red_version > version("6.0.0") then
+        remove_redis_user(red)
+      end
     end)
 
     it("connection pool respects database setting", function()
-      local red = redis:new()
-      red:set_timeout(2000)
-
-      finally(function()
-        if red then
-          red:close()
-        end
-      end)
-
       assert(red:connect(REDIS_HOST, REDIS_PORT))
 
       assert(red:select(REDIS_DB_1))
@@ -227,25 +227,35 @@ describe("Plugin: rate-limiting (integration)", function()
     end)
 
     it("authenticates and executes with a valid redis user having proper ACLs", function()
-      local res = assert(client:send {
-        method = "GET",
-        path = "/status/200",
-        headers = {
-          ["Host"] = "redistest3.com"
-        }
-      })
-      assert.res_status(200, res)
+      if red_version >= version("6.0.0") then
+        local res = assert(client:send {
+          method = "GET",
+          path = "/status/200",
+          headers = {
+            ["Host"] = "redistest3.com"
+          }
+        })
+        assert.res_status(200, res)
+      else
+        ngx.log(ngx.WARN, "Redis v" .. tostring(red_version) .. " does not support ACL functions " ..
+          "'authenticates and executes with a valid redis user having proper ACLs' will be skipped")
+      end
     end)
 
     it("fails to rate-limit for a redis user with missing ACLs", function()
-      local res = assert(client:send {
-        method = "GET",
-        path = "/status/200",
-        headers = {
-          ["Host"] = "redistest4.com"
-        }
-      })
-      assert.res_status(500, res)
+      if red_version >= version("6.0.0") then
+        local res = assert(client:send {
+          method = "GET",
+          path = "/status/200",
+          headers = {
+            ["Host"] = "redistest4.com"
+          }
+        })
+        assert.res_status(500, res)
+      else
+        ngx.log(ngx.WARN, "Redis v" .. tostring(red_version) .. " does not support ACL functions " ..
+          "'fails to rate-limit for a redis user with missing ACLs' will be skipped")
+      end
     end)
 
   end)
